77c6bacfffdabfe0d26d494d9ec5a77dc586a22e
areebbaig580 <areebbaig580@gmail.com>
2026-06-27 20:49:20 +0530

    Setup router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import { SquareCheck } from 'lucide-react'
+import {SquareCheck } from 'lucide-react'
 import { ChartSpline } from 'lucide-react'
-import Timer from './Component/Home/timer'
-import TimerState from './Component/Home/TimerState'
-import Nav from './Component/Home/Nav'
+import Timer from './Component/Pages/home/timer'
+import TimerState from './Component/Pages/home/TimerState'
+import Nav from './Component/Pages/home/Nav'
+import { Route,Routes } from 'react-router-dom'
+import Dashboard from './Component/Pages/dashboard/dashboard'
+import Home from './Component/Pages/home/home'
+import Settings from './Component/Pages/settings/Settings'
 
 const App = () => {
-  const [timerState, setTimerstate] = useState(25 * 60);
-
+  
   return (
-    <div className='h-screen w-screen bg-black flex flex-col items-center text-white'>
-      <Nav />
-      <div className='h-[70vh] w-[65vw] bg-[#1b1a1ab7] rounded-xl flex  items-center'>
-        <TimerState setTimerstate={setTimerstate} />
-        <Timer timerState={timerState} />
-      </div>
+    <div className='h-screen w-screen bg-black text-white'>
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/dashboard' element= {<Dashboard/>}/>
+        <Route path='/setting' element= {<Settings/>}/>
+      </Routes>
     </div>
   )
 }
