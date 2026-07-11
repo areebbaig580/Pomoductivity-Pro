e1a7f809b3c80d0625d0ade76deebdcf0e82b0e0
areebbaig580 <areebbaig580@gmail.com>
2026-07-11 23:10:18 +0530

    Initialise dashboard

---
+++ src/Component/Pages/dashboard/dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className=' h-full w-full flex justify-center '>
+      <div className='h-fit w-6/10 bg-secondary text-text-purple py-4 px-4 font-bold text-xl rounded-xl mt-4'>
+        Dashboard</div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
