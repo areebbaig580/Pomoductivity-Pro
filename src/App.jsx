3950df25a27797fdfe7dfef8be12ca03986401a2
areebbaig580 <areebbaig580@gmail.com>
2026-07-13 02:00:25 +0530

    Organised file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { SquareCheck } from 'lucide-react'
-import { ChartSpline } from 'lucide-react'
-import Timer from './Component/Pages/home/timer'
-import TimerState from './Component/Pages/home/TimerState'
-import Nav from './Component/Pages/home/Nav'
 import { Route, Routes } from 'react-router-dom'
-import Dashboard from './Component/Pages/dashboard/dashboard'
-import Home from './Component/Pages/home/home'
-import Settings from './Component/Pages/settings/Settings'
+import Dashboard from './pages/dashboard'
+import Home from './pages/home'
+import Settings from './pages/Settings'
 
 const App = () => {
   let theme = JSON.parse(localStorage.getItem('theme'));
@@ -23,7 +18,7 @@ const App = () => {
   const [notifyValue, setNotifyValue] = useState(5);
 
   return (
-    <div className='h-screen w-screen bg-bg text-text'>
+    <div className='min-h-screen w-full bg-bg text-text'>
       <Routes>
         <Route path='/' element={<Home
           notifyValue={notifyValue}
